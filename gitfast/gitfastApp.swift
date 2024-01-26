71f2af3d8858296b6eaf8d2df474be7ee2187ea6
Ignacio <1992ignacio@gmail.com>
2024-01-26 04:18:45 -0800

    Initial Commit

---
+++ gitfast/gitfastApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitfastApp.swift
+//  gitfast
+//
+//  Created by Ignacio Arias on 2024-01-26.
+//
+
+import SwiftUI
+
+@main
+struct gitfastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
